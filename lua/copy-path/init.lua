f1305c09492f265ece27f18fc332bca2ccd8c724
Felipe Sánchez Soberanis <felipesaso2000@gmail.com>
2024-03-30 11:26:23 -0600

    Copy path from javascript and typescript

--- lua/copy-path/init.lua
+++ lua/copy-path/init.lua
@@ -67,4 +67,57 @@ M.copy_json_path = function(args)
     print("Path " .. path .. " copied to register " .. register)
 end
 
+M.copy_javascript_path = function(args)
+    local register = args.register
+
+    local current_node = get_current_node()
+    local root_node = get_root_node()
+
+    local searchable_node_types = {
+        ["string"] = true,
+        ["number"] = true,
+        ["true"] = true,
+        ["false"] = true,
+        ["object"] = true,
+        ["array"] = true,
+        ["null"] = true,
+        ["undefined"] = true,
+        ["arrow_function"] = true,
+        ["identifier"] = true
+    }
+
+    local traversed_nodes = {}
+    local prev_node
+
+    while current_node ~= root_node and current_node:type() ~=
+        "variable_declarator" do
+        if (current_node:type() == "pair") then
+            local key_node = current_node:named_children()[1]
+            local key_name = ts.get_node_text(key_node, 0)
+            table.insert(traversed_nodes, key_name)
+        elseif searchable_node_types[current_node:type()] then
+            if current_node:type() == "array" and prev_node then
+                for i, child_node in ipairs(current_node:named_children()) do
+                    if child_node == prev_node then
+                        table.insert(traversed_nodes, "[" .. i - 1 .. "]")
+                        prev_node = nil
+                        break
+                    end
+                end
+            end
+            prev_node = current_node
+        end
+        current_node = current_node:parent()
+    end
+
+    if current_node:type() == "variable_declarator" then
+        local name_node = current_node:named_children()[1]
+        table.insert(traversed_nodes, ts.get_node_text(name_node, 0))
+    end
+
+    local path = traversed_nodes_to_path(traversed_nodes)
+    vim.fn.setreg(register, path)
+    print("Path " .. path .. " copied to register " .. register)
+end
+
 return M
